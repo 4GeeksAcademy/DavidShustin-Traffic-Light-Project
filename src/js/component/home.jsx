2f73ed2423824812a5dea710029f1bf7e247b253
DavidShustin <167934750+DavidShustin@users.noreply.github.com>
2024-09-09 19:10:41 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,59 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [selectedColor, setSelectedColor] = useState("null");
+	const [cycleActive, setCycleActive] = useState(false);
+
+	const cycleTrafficColors = () => {
+		if (cycleActive) {
+			if (selectedColor === "green" ) {
+				setSelectedColor("yellow");
+			} else if (selectedColor === "yellow") {
+				setSelectedColor("red");
+			} else if (selectedColor === "red") {
+				setSelectedColor("green");
+			}
+		}
+	}
+	const startStopCycle = () => {
+		setCycleActive(!cycleActive);
+		if (!cycleActive) {
+			setSelectedColor("green");
+		}
+	};
+
+	useEffect(() => {
+		const intervalId = setInterval(cycleTrafficColors, 1800);
+		return () => clearInterval(intervalId);
+	}, [selectedColor])
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-center">
+			<div className="traffic-light-container">
+				<div className="traffic-light-top">
+
+				</div>
+				<div className="traffic-light">
+					<div
+						onClick={() => setSelectedColor("red")}
+						className={"light red" + ((selectedColor === "red") ? " glow" : "")}>
+					</div>
+					<div
+						onClick={() => setSelectedColor("yellow")}
+						className={"light yellow" + ((selectedColor === "yellow") ? " glow" : "")}>
+					</div>
+					<div
+						onClick={() => setSelectedColor("green")}
+						className={"light green" + ((selectedColor === "green") ? " glow" : "")}>
+					</div>
+				</div>
+				<div className="d-flex justify-content-center">
+					<button className="btn btn-dark" onClick={startStopCycle}  id="cycleButton">{cycleActive ? "stop traffic light cycle": "start traffic light cycle"}</button>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,7 @@
-//import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom/client";
-
-// include your styles into the webpack bundle
+import ReactDOM from "react-dom";
+import Home from "./component/home.jsx";
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
+ReactDOM.render(<Home />, document.querySelector("#app"));
